d360aadd28d8a33c8803629518f35a9e259966c3
Iván García Vicario <ivan.garcia@externos.correo.gob.es>
2019-02-07 16:16:42 +0100

    primer commit repositorio 1

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -4,6 +4,10 @@ package sample;
  * Created by chamilad on 9/5/14.
  */
 public class HelloWorld {
+	
+	private int dameUno() {
+		return 1;
+	}
 
     public static String getMessage() {
         return "Hello World Tomcat!!";

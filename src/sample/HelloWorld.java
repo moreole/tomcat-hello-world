d73d215c5032eb3b24cc57b1deb0ba03c8f4dafe
Iván García Vicario <ivan.garcia@externos.correo.gob.es>
2019-02-12 14:44:12 +0100

    Liada 2

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -13,7 +13,11 @@ public class HelloWorld {
         return "Hello World Tomcat!!";
     }
     
+    private static void jul() {
+    	System.out.println("lo");
+    }
+    
     private void metodo2(int nada) {
-    	System.out.println("proyecto 2");
+    	System.out.println("proyecto 1_0212");
     }
 }

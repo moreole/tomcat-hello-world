bdc9c0488d4dc649c474248e366703a5e584e75e
Iván García Vicario <ivan.garcia@externos.correo.gob.es>
2019-02-14 17:04:44 +0100

    Añadir utilidad para calcular diferencias entre ficheros

---
+++ src/util/CompararArchivos.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CompararArchivos {
+	
+	public static void propertiesKeyRepetidos(String filePath) throws IOException {
+		Queue<String> restantes = new LinkedList<>(Files.readAllLines(Paths.get(filePath)));
+		for (int i=restantes.size(); i > 0; i++) {
+			try {
+				String elemento = restantes.poll().split("=")[0] + "=";
+				if (restantes.stream().anyMatch(e2 -> e2.startsWith(elemento))) {
+					System.out.println(elemento);
+				}
+			} catch (Exception e) {}
+		}
+	}
+
+}
